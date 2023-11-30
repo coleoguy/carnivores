23158cd5bbb3fb5a9f4349c03cad075d81b62d49
Michelle Jonika <Michelle19@tamu.edu>
2023-11-30 11:41:57 -0600

    Update results

--- scripts/body_mass/carn_chrom_bm_analysis.R
+++ scripts/body_mass/carn_chrom_bm_analysis.R
@@ -70,7 +70,7 @@ temp <- mcmc(lik = con.lik,
              x.init = runif(6, 0, 1),
              prior = prior,
              w = 10,
-             nsteps = 500,
+             nsteps = 1000,
              upper = 20,
              lower = 0)
 
@@ -84,7 +84,7 @@ w <- diff(sapply(tune[2:7],
 # w <- c(14.44759, 12.13979, 16.06275, 14.23117, 2.477789, 3.194005)
 
 # register cores to use in parallel
-registerDoMC(detectCores(all.tests = T) - 4)
+registerDoMC(detectCores(all.tests = T) - 30)
 
 #create empty list to store results
 result <- list()
@@ -114,6 +114,112 @@ x1 <- foreach(i = 1:100) %dopar%{
 }
 save(x1, file="../../results/body_mass/trial1_bm.Rdata")
 
+x2 <- foreach(i = 1:100) %dopar%{
+  # make the basic likelihood function for the data
+  lk.mk <- make.mkn(trees[[i]], states = datalist[[i]],
+                    k = ncol(datalist[[i]]), strict = F,
+                    control = list(method = "ode"))
+  # now we constrain our model to be biologically realistic for
+  # chromosomes.
+  con.lk.mk<-constrainMkn(data = datalist[[i]], lik = lk.mk, hyper = T,
+                          polyploidy = F, verbose = F,
+                          constrain = list(drop.demi = T, drop.poly = T))
+  # now we are ready to run our inference run
+  result[[i]] <- mcmc(con.lk.mk,
+                      x.init =  runif(6, 0, 10),
+                      prior = prior,
+                      w = w,
+                      nsteps = iter,
+                      upper = 50,
+                      lower = 0)
+}
+save(x2, file="../../results/body_mass/trial2_bm.Rdata")
+
+x3 <- foreach(i = 1:100) %dopar%{
+  # make the basic likelihood function for the data
+  lk.mk <- make.mkn(trees[[i]], states = datalist[[i]],
+                    k = ncol(datalist[[i]]), strict = F,
+                    control = list(method = "ode"))
+  # now we constrain our model to be biologically realistic for
+  # chromosomes.
+  con.lk.mk<-constrainMkn(data = datalist[[i]], lik = lk.mk, hyper = T,
+                          polyploidy = F, verbose = F,
+                          constrain = list(drop.demi = T, drop.poly = T))
+  # now we are ready to run our inference run
+  result[[i]] <- mcmc(con.lk.mk,
+                      x.init =  runif(6, 0, 10),
+                      prior = prior,
+                      w = w,
+                      nsteps = iter,
+                      upper = 50,
+                      lower = 0)
+}
+save(x3, file="../../results/body_mass/trial3_bm.Rdata")
+
+x4 <- foreach(i = 1:100) %dopar%{
+  # make the basic likelihood function for the data
+  lk.mk <- make.mkn(trees[[i]], states = datalist[[i]],
+                    k = ncol(datalist[[i]]), strict = F,
+                    control = list(method = "ode"))
+  # now we conswget -r -c -nH --cut-dirs=1 --no-parent --reject "index.html*" https://gc3fstorage.uoregon.edu/r64047_20231116_221949/7627/reads/train our model to be biologically realistic for
+  # chromosomes.
+  con.lk.mk<-constrainMkn(data = datalist[[i]], lik = lk.mk, hyper = T,
+                          polyploidy = F, verbose = F,
+                          constrain = list(drop.demi = T, drop.poly = T))
+  # now we are ready to run our inference run
+  result[[i]] <- mcmc(con.lk.mk,
+                      x.init =  runif(6, 0, 10),
+                      prior = prior,
+                      w = w,
+                      nsteps = iter,
+                      upper = 50,
+                      lower = 0)
+}
+save(x4, file="../../results/body_mass/trial4_bm.Rdata")
+
+x5 <- foreach(i = 1:100) %dopar%{
+  # make the basic likelihood function for the data
+  lk.mk <- make.mkn(trees[[i]], states = datalist[[i]],
+                    k = ncol(datalist[[i]]), strict = F,
+                    control = list(method = "ode"))
+  # now we constrain our model to be biologically realistic for
+  # chromosomes.
+  con.lk.mk<-constrainMkn(data = datalist[[i]], lik = lk.mk, hyper = T,
+                          polyploidy = F, verbose = F,
+                          constrain = list(drop.demi = T, drop.poly = T))
+  # now we are ready to run our inference run
+  result[[i]] <- mcmc(con.lk.mk,
+                      x.init =  runif(6, 0, 10),
+                      prior = prior,
+                      w = w,
+                      nsteps = iter,
+                      upper = 50,
+                      lower = 0)
+}
+save(x5, file="../../results/body_mass/trial5_bm.Rdata")
+
+x6 <- foreach(i = 1:100) %dopar%{
+  # make the basic likelihood function for the data
+  lk.mk <- make.mkn(trees[[i]], states = datalist[[i]],
+                    k = ncol(datalist[[i]]), strict = F,
+                    control = list(method = "ode"))
+  # now we constrain our model to be biologically realistic for
+  # chromosomes.
+  con.lk.mk<-constrainMkn(data = datalist[[i]], lik = lk.mk, hyper = T,
+                          polyploidy = F, verbose = F,
+                          constrain = list(drop.demi = T, drop.poly = T))
+  # now we are ready to run our inference run
+  result[[i]] <- mcmc(con.lk.mk,
+                      x.init =  runif(6, 0, 10),
+                      prior = prior,
+                      w = w,
+                      nsteps = iter,
+                      upper = 50,
+                      lower = 0)
+}
+save(x6, file="../../results/body_mass/trial6_bm.Rdata")
+
+
 ##### Checking for convergence ###########
 # After checking runs I found that some runs stayend in a low prob
 # area with high rates in clades that have high pop sizes. To see if these
@@ -126,7 +232,7 @@ uncon1 <- c()
 for(i in 1:100){
   #looks at the mean rates of the two ascending rates from the model to see if 
   #they are less than 0
-  if(mean(x1[[i]]$asc2[451:500])-mean(x1[[i]]$asc1[451:500]) < 0){
+  if(mean(x1[[i]]$asc2[900:1000])-mean(x1[[i]]$asc1[900:1000]) < 0){
     #stores the current run that didn't meet convergence
     uncon1 <- c(uncon1, i)
   }
--- scripts/body_mass/result_munging_bm.R
+++ scripts/body_mass/result_munging_bm.R
@@ -4,13 +4,37 @@
 #the final posterior dataset for visualization
 
 #load in each of the replicates
-load("../scripts/trial1_bm.Rdata")
+load("../results/body_mass/trial1_bm.Rdata")
+load("../results/body_mass/trial2_bm.Rdata")
+load("../results/body_mass/trial3_bm.Rdata")
+load("../results/body_mass/trial4_bm.Rdata")
+load("../results/body_mass/trial5_bm.Rdata")
+load("../results/body_mass/trial6_bm.Rdata")
+
+#create an empty 
+results <- list()
+
+#make a list of the replicates
+trials <- list(x1,x2,x3,x4,x5,x6)
+
+#loop that retains the MCMC run with the highest final posterior probability
+for(i in 1:length(x1)){
+  #loops through each tree and stores the final posterior probability
+  liks <- c(trials[[1]][[i]]$p[500],
+            trials[[2]][[i]]$p[500],
+            trials[[3]][[i]]$p[500],
+            trials[[4]][[i]]$p[500],
+            trials[[5]][[i]]$p[500],
+            trials[[6]][[i]]$p[500])
+  #store the tree with the highest posterior probabiliyt for the ifinal dataset
+  results[[i]] <- trials[[which.max(liks)]][[i]]
+}
 
 #load in the ttree depths to transform back into units of millions of years (MY)
 tree.depths <- read.csv("../data/body_mass/bm_treedepths.csv")
 
 #only process post burn-in results
-post.burn <- results[[1]][451:500, 2:8]
+post.burn <- x1[[1]][900:1000, 2:8]
 
 #transform the post burn in results back into MY from the tree depths
 post.burn[,1:6] <- post.burn[,1:6]/tree.depths[1,2]
@@ -18,7 +42,7 @@ post.burn[,1:6] <- post.burn[,1:6]/tree.depths[1,2]
 #loop that organizes rates into a pretty table
 for(i in 2:100){
   #pulls results for specific tree
-  temp <- results[[i]]
+  temp <- x1[[i]]
   #transforms the rates back by their tree depths
   temp[,2:7] <- temp[,2:7]/tree.depths[i,2]
   ##bind in post-burn-in samples from each ttree after they have been back 
@@ -27,5 +51,5 @@ for(i in 2:100){
 }
 
 #save the results output
-write.csv(post.burn, file="../results/body_mass/initial_bm.csv")
+write.csv(post.burn, file="../results/body_mass/bm.csv")
 
